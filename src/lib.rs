b8fbce1e02f960a896e371dbd356d2c72518e871
Pierre Lairez <pierre@lairez.fr>
2026-05-02 22:48:56 +0200

    per-file bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,23 @@
 #![allow(non_snake_case)]
 #![allow(unnecessary_transmutes)]
 
-pub mod ffi_prelude;
+pub use libc::{
+    c_char, c_double, c_int, c_long, c_longlong, c_schar, c_short, c_uchar, c_uint, c_ulong,
+    c_ulonglong, c_ushort, c_void,
+};
 
-include!(concat!(env!("OUT_DIR"), "/types.rs"));
+pub use libc::FILE;
+
+pub use libc::{
+    pthread_attr_t, pthread_barrier_t, pthread_barrierattr_t, pthread_cond_t, pthread_condattr_t,
+    pthread_key_t, pthread_mutex_t, pthread_mutexattr_t, pthread_once_t, pthread_rwlock_t,
+    pthread_rwlockattr_t, pthread_spinlock_t, pthread_t,
+};
+
+pub type size_t = libc::size_t;
+pub type ssize_t = libc::ssize_t;
+
+pub type __va_list_tag = u64;
+
+
+include!(concat!(env!("OUT_DIR"), "/flint.rs"));
